da7056b41eb75487a9d018ce7067fbd85fbdf601
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-03-02 09:41:52 +0530

    create chat and user model
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ models/chatModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema(
+  {
+    sender_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    is_online: {
+      type: String,
+      default: "0",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", chatSchema);
